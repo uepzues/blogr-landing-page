fc278f32226cae03e154ae9950bd25739d39dfb1
zuesuep <kimsanz@gmail.com>
2023-10-20 23:19:45 +0800

    modified:   src/components/Section2.jsx

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -2,7 +2,7 @@ export default function Section2() {
   return (
     <>
       <section className="section2 w-full h-[40rem] bg-section-bgnd bg-no-repeat bg-[-40%_85%] overflow-visible relative rounded-bl-[100px] rounded-tr-[100px] mt-[-27rem] scrn900:my-[5rem] scrn900:h-[70vh] scrn900:bg-[50%_150%] scrn600:h-[80vh] scrn400:mt-[12rem]">
-        <div className="grid grid-cols-2 max-w-[1150px] left-[0] right-[0] mx-[2rem] top-[-50px] gap-10 overflow-visible absolute scrn900:flex scrn900:flex-col ">
+        <div className="grid grid-cols-2 max-w-[1150px] left-[0] right-[0] mx-[calc(10%)] top-[-50px] gap-10 overflow-visible absolute scrn900:flex scrn900:flex-col ">
           <div className="img scrn900:relative">
             <img
               className="ms-[-40px] scrn900:absolute scrn900:ms-0 scrn900:w-[] scrn900:left-[50%] scrn900:translate-x-[-50%] scrn900:top-[-13rem] scrn600:top-[-10rem]"
