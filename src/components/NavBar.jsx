ede7961e476a26bb3c873d15591372ba97c5f61c
zuesuep <kimsanz@gmail.com>
2023-10-20 08:37:17 +0800

    made mobile friendly adn added mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,10 @@ function NavBar() {
   const [menu, setMenu] = useState(false);
   const [menu2, setMenu2] = useState(false);
   const [menu3, setMenu3] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [mobileMenu2, setMobileMenu2] = useState(false);
+  const [mobileMenu3, setMobileMenu3] = useState(false);
+  const [mobileBurger, setMobileBurger] = useState(false)
   const [nav, setNav] = useState(false);
 
   useEffect(() => {
@@ -13,29 +17,48 @@ function NavBar() {
       setNav(newWidth > 900 ? false : true);
     };
 
+    window.addEventListener("load", handleResize);
     window.addEventListener("resize", handleResize);
 
     return () => {
+      window.addEventListener("load", handleResize);
       window.removeEventListener("resize", handleResize);
     };
   });
 
+    const toggleVisibility = ()=>{
+    setMobileMenu(!mobileMenu)
+  }
+  const toggleVisibility2 = ()=>{
+    setMobileMenu2(!mobileMenu2)    
+  }
+  const toggleVisibility3 = ()=>{
+    setMobileMenu3(!mobileMenu3)    
+  }
+  const visBurger = ()=>{
+    setMobileBurger(!mobileBurger)
+  }
+
+
   return (
     <>
-      <nav className={`w-screen ${nav ? "visible" : "hidden"}`}>
-        <div className="navMobile flex justify-between items-center mx-[20px] mt-[4rem]">
+      <nav className={`w-screen ${nav ? "visible" : "hidden"} `}>
+        <div className="navMobile flex justify-between items-center mx-[20px] mt-[4rem]  relative">
           <div>
             <img src="../images/logo.svg" alt="logo image" />
           </div>
-          <div className="cursor-pointer">
+          <div className="cursor-pointer"
+          onClick={visBurger}>
             <img src="../images/icon-hamburger.svg" alt="menu" />
           </div>
         </div>
 
-        <div className="modal rounded-lg bg-white text-[#4b5862] mx-[2rem] mt-[2rem] text-[1.6rem] font-semibold">
+        <div className={`modal ${mobileBurger?'visible': 'hidden'} absolute left-0 right-0 z-20 rounded-lg bg-white text-[#4b5862] mx-[2rem] mt-[2rem] text-[1.6rem] font-semibold`}>
           <ul>
-            <li className="">
-              <div className="flex items-center justify-center pt-[2rem] ">
+            <li className="flex flex-col items-center justify-center h-auto">
+              <div className="flex items-center mt-[5rem] cursor-pointer"
+              onClick={toggleVisibility3}
+              >
                 Product
                 <img
                   className="ms-4"
@@ -43,9 +66,27 @@ function NavBar() {
                   alt="arrow"
                 />
               </div>
+              <br />
+              <ul
+                className={`lv2 bg-[ff8f70] h-auto ${mobileMenu3?'block':'hidden'} static w-[80%] text-center bg-slate-200 pl-[unset]`}
+              >
+                <NavList
+                  list2={[
+                    "Overview",
+                    "Pricing",
+                    "Marketplace",
+                    "Features",
+                    "Integrations",
+                  ]}
+                />
+              </ul>
             </li>
-            <li>
-              <div className="flex items-center justify-center pt-[2rem] ">
+
+            <li className="flex flex-col items-center justify-center h-auto ">
+              <div className="flex items-center mt-[2rem] cursor-pointer"
+              onClick={toggleVisibility2}
+
+              >
                 Company
                 <img
                   className="ms-4"
@@ -53,9 +94,17 @@ function NavBar() {
                   alt="arrow"
                 />
               </div>
+              <br />
+              <ul
+                className={`lv2  ${mobileMenu2?'block':'hidden'} bg-[ff8f70] h-auto static w-[80%] text-center bg-slate-200 pl-[unset]`}
+              >
+                <NavList list2={["About", "Team", "Blog", "Careers"]} />
+              </ul>
             </li>
-            <li>
-              <div className="flex items-center justify-center pt-[2rem] ">
+            <li className="flex flex-col items-center justify-center h-auto ">
+              <div className="flex items-center mt-[2rem] cursor-pointer"
+                onClick={toggleVisibility}
+              >
                 Connect
                 <img
                   className="ms-4"
@@ -63,8 +112,22 @@ function NavBar() {
                   alt="arrow"
                 />
               </div>
+              <br />
+              <ul
+                className={`lv2 ${mobileMenu?'block':'hidden'} bg-[ff8f70] h-auto static w-[80%] text-center bg-slate-200 pl-[unset]`}
+              >
+                <NavList list2={["Contact", "Newsletter", "LinkedIn"]} />
+              </ul>
             </li>
           </ul>
+          <hr className="mt-[3rem] mb-[2rem] w-[80%]" />
+          <div className="flex flex-col justify-center items-center pb-[3rem]">
+            <p className="me-8 flex items-center mb-[2rem]">Login</p>
+            <ButtonComp
+              text="Sign Up"
+              className="text-white me-8 bg-gradient-to-r from-[#ff7a85] to-[#ff525d] hover:bg-[#ff7a85] hover:text-white"
+            />
+          </div>
         </div>
       </nav>
       <nav
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,17 +1,17 @@
 export default function Section2() {
   return (
     <>
-      <div className="section2 items-center w-full h-[37rem] bg-section-bgnd bg-no-repeat bg-[-40%_85%] overflow-visible relative rounded-bl-[100px] rounded-tr-[100px] my-[10rem] scrn900:my-[5rem] scrn900:h-[40vh] scrn900:bg-[50%_150%]"> 
+      <div className="section2 items-center w-full h-[37rem] bg-section-bgnd bg-no-repeat bg-[-40%_85%] overflow-visible relative rounded-bl-[100px] rounded-tr-[100px] my-[10rem] scrn900:my-[5rem] scrn900:h-[60vh] scrn900:bg-[50%_150%] scrn600:h-[50vh]">
         <div className="grid grid-cols-2 max-w-[1000px] left-[50px] right-[50px] mx-auto top-[-50px] gap-10 overflow-visible absolute scrn900:flex scrn900:flex-col ">
           <div className="img scrn900:relative">
             <img
-            className="ms-[-40px] scrn900:absolute scrn900:ms-0 scrn900:w-[60%] scrn900:left-[50%] scrn900:translate-x-[-50%] scrn900:top-[-13rem] scrn600:top-[-5rem] scrn600:w-[300px]"
+              className="ms-[-40px] scrn900:absolute scrn900:ms-0 scrn900:w-[60%] scrn900:left-[50%] scrn900:translate-x-[-50%] scrn900:top-[-13rem] scrn600:top-[-5rem] scrn600:w-[30rem]"
               src="../images/illustration-phones.svg"
               alt="phone images"
             />
           </div>
-          <div className="content my-auto text-white scrn900:mt-[20vh] scrn900:text-center">
-            <h2 className="text-[3.4rem]  font-bold">
+          <div className="content my-auto text-white scrn900:mt-[30vh] scrn900:text-center scrn600:mt-[23vh]">
+            <h2 className="text-[3.4rem] font-bold">
               State of the Art Infrastructure
             </h2>
             <p className="text-[1.6rem] font-light mt-10">
